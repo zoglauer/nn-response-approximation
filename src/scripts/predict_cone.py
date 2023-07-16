4020cb87fb1f2a1729901c5788097d583bce4c6d
22anirudhk <akotamraju@gmail.com>
2023-07-16 13:37:51 -0700

    batch

--- src/scripts/predict_cone.py
+++ src/scripts/predict_cone.py
@@ -60,7 +60,7 @@ config = {
     "DEPTH": 36,  # 180 / 5
     "train_pct": 0.7,
     "val_pct": 0.15,
-    "BATCH_SIZE": 1,
+    "BATCH_SIZE": 32,
     # ------------------- #
     "EPOCHS": 1000,
     "PATIENCE": 50,
