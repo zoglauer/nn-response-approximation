19ea017d7aee6def8ad39a977c729af1e02785b8
22anirudhk <akotamraju@gmail.com>
2023-07-16 01:34:02 -0700

    create script

---
+++ src/scripts/crosssec.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+# Remember:
+# Submit via: sbatch ...
+# Check via: squeue -u $USER
+
+#SBATCH -J Python
+
+#SBATCH --account=fc_cosi
+#SBATCH --partition=savio2_gpu
+#SBATCH --qos=savio_normal
+
+#SBATCH -t 24:00:00
+
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=2
+#SBATCH --gres=gpu:1
+
+#SBATCH --signal=2@60
+
+# --> CHANGE TO YOUR EMAIL
+
+##SBATCH --akotamraju@berkeley.edu
+
+##SBATCH --mail-type=ALL
+
+export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK
+
+echo "Starting analysis on host ${HOSTNAME} with job ID ${SLURM_JOB_ID}..."
+
+echo "Loading modules..."
+module load nano
+module load gcc
+module load python
+
+echo "Starting execution..."
+
+python3 create_cross_sec.py
+
+echo "Waiting for all processes to end..."
+wait
