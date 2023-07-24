928373eafad31aa7ae431d1e40358f4c4214cb0f
22anirudhk <akotamraju@gmail.com>
2023-07-23 23:51:46 -0700

    update savio run

--- src/scripts/saviorun.sh
+++ src/scripts/saviorun.sh
@@ -12,10 +12,10 @@
 
 #SBATCH -t 10:00:00
 
-#SBATCH --nodes=1
+#SBATCH --nodes=3
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=4
-#SBATCH --gres=gpu:TITAN:8
+#SBATCH --gres=gpu:TITAN:24
 
 #SBATCH --signal=2@60
 
