dc007009ef8e24f3da03e6e907d9834b808606e1
22anirudhk <akotamraju@gmail.com>
2023-07-23 19:25:00 -0700

    fix percentage

--- src/scripts/predict_cone.py
+++ src/scripts/predict_cone.py
@@ -60,8 +60,8 @@ config = {
     "NORMALIZE": False,
     # ------------------- #
     "DEPTH": 36,  # 180 / 5
-    "train_pct": 0.15,
-    "val_pct": 0.15,
+    "train_pct": 0.95,
+    "val_pct": 0.04,
     "BATCH_SIZE": 32,
     # ------------------- #
     "EPOCHS": 1000,
