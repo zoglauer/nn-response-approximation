bcac5402d7f04ad018e4d2714bc17680fad07771
22anirudhk <akotamraju@gmail.com>
2023-07-23 23:59:19 -0700

    try gpu again

--- src/scripts/saviorun.sh
+++ src/scripts/saviorun.sh
@@ -12,10 +12,10 @@
 
 #SBATCH -t 10:00:00
 
-#SBATCH --nodes=1
+#SBATCH --nodes=2
 #SBATCH --ntasks-per-node=1
-#SBATCH --cpus-per-task=8
-#SBATCH --gres=gpu:A40:8
+#SBATCH --cpus-per-task=4
+#SBATCH --gres=gpu:TITAN:8
 
 #SBATCH --signal=2@60
 
