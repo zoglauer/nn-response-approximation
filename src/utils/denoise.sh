6e4186f50aca69fbd4c8385bb3acb58fbbd216cd
22anirudhk <akotamraju@gmail.com>
2023-07-25 21:51:06 -0700

    add denoise.sh

---
+++ src/utils/denoise.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+# Remember:
+# Submit via: sbatch ...
+# Check via: squeue -u $USER
+
+#SBATCH -J Python
+
+#SBATCH --account=fc_cosi
+#SBATCH --partition=savio3_gpu
+#SBATCH --qos=savio_lowprio
+
+#SBATCH -t 24:00:00
+
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=4
+#SBATCH --gres=gpu:TITAN:8
+
+#SBATCH --signal=2@60
+
+# --> CHANGE TO YOUR EMAIL
+
+##SBATCH --akotamraju@berkeley.edu
+
+##SBATCH --mail-type=ALL
+
+export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK
+
+echo "Starting analysis on host ${HOSTNAME} with job ID ${SLURM_JOB_ID}..."
+
+echo "Loading modules..."
+module load nano
+module load gcc
+module load python
+
+echo "Starting execution..."
+
+python3 create_denoised_data.py
+
+echo "Waiting for all processes to end..."
+wait
